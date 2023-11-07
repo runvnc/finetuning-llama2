09b80af70fed9e532c1507768e656f5bfbc17dbb
Jason Livesay <runvnc@gmail.com>
2023-11-07 23:00:44 +0000

    naming

--- train_on_s3_dataset.py
+++ train_on_s3_dataset.py
@@ -6,16 +6,13 @@ import train
 def main():
     parser = argparse.ArgumentParser(
         description='This script fine-tunes an LLM on the specified dataset in s3.',
-        usage='python train_on_s3_dataset.py <s3_path>'
+        usage='./train_on_s3_dataset.py <s3_path>'
     )
-    parser.add_argument('url_list_filename', type=str, help='The name of the file containing the URLs of the web pages in the dataset. Each URL should be on a new line.')
-    parser.add_argument('s3_path', type=str, help='The path to the S3 bucket where the dataset will be stored.')
+    parser.add_argument('s3_partial_path', type=str, 
+      help='The partial path name to the S3 bucket where the dataset will be stored (off of the default bucket).')
     args = parser.parse_args()
 
-    with open(args.filename, 'r') as f:
-        urls = [line.strip() for line in f]
-
-    dataset.store_url_dataset('meta-llama/Llama-2-7b-hf', args.s3_path, urls)
+    train.fine_tune(args.s3_partial_path, 'meta-llama/Llama-2-7b-hf')
 
 if __name__ == "__main__":
     main()
--- upload_web_dataset_to_s3.py
+++ upload_web_dataset_to_s3.py
@@ -6,16 +6,16 @@ import dataset
 def main():
     parser = argparse.ArgumentParser(
         description='This script loads URLs from a specified file, extracts the text from those web pages, and stores the dataset to a specified S3 bucket path.',
-        usage='python upload_web_dataset_to_s3.py <url_list_filename> <s3_path>'
+        usage='./upload_web_dataset_to_s3.py <url_list_filename> <s3_path_name>'
     )
     parser.add_argument('url_list_filename', type=str, help='The name of the file containing the URLs of the web pages in the dataset. Each URL should be on a new line.')
-    parser.add_argument('s3_path', type=str, help='The path to the S3 bucket where the dataset will be stored.')
+    parser.add_argument('s3_path_name', type=str, help='The partial name in S3 where the dataset will be stored (in the default bucket).')
     args = parser.parse_args()
 
-    with open(args.filename, 'r') as f:
+    with open(args.url_list_filename, 'r') as f:
         urls = [line.strip() for line in f]
 
-    dataset.store_url_dataset('meta-llama/Llama-2-7b-hf', args.s3_path, urls)
+    dataset.store_url_dataset('meta-llama/Llama-2-7b-hf', args.s3_path_name, urls)
 
 if __name__ == "__main__":
     main()
