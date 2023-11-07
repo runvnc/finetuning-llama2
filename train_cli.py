23933bd33b6bde335958c67d18dc549010e6e6db
Jason Livesay <runvnc@gmail.com>
2023-11-07 02:45:08 +0000

    aider: Updated `train_cli.py` to include usage information for the script.
    # Aider chat conversation:
    
    USER: please update `train_cli.py` so that it appropriately can print its usage information
    
    ASSISTANT: To add usage information to the `train_cli.py` script, we can add a more detailed description and help text for each argument in the `argparse.ArgumentParser` function.
    
    Here is the *edit block* for the changes:
    
    ```python
    train_cli.py
    <<<<<<< HEAD
    import argparse
    import train
    
    def main():
        parser = argparse.ArgumentParser(description='Load URLs and store dataset.')
        parser.add_argument('filename', type=str, help='File containing URLs')
        parser.add_argument('s3_path', type=str, help='S3 bucket path')
        args = parser.parse_args()
    
        # Load URLs from file
        with open(args.filename, 'r') as f:
            urls = [line.strip() for line in f]
    
        # Call store_url_dataset function
        train.store_url_dataset('meta-llama/Llama-2-7b-hf', args.s3_path, urls)
    
    if __name__ == "__main__":
        main()
    =======
    import argparse
    import train
    
    def main():
        parser = argparse.ArgumentParser(
            description='This script loads URLs from a specified file and stores the dataset to a specified S3 bucket path.',
            usage='python train_cli.py <filename> <s3_path>'
        )
        parser.add_argument('filename', type=str, help='The name of the file containing the URLs. Each URL should be on a new line.')
        parser.add_argument('s3_path', type=str, help='The path to the S3 bucket where the dataset will be stored.')
        args = parser.parse_args()
    
        # Load URLs from file
        with open(args.filename, 'r') as f:
            urls = [line.strip() for line in f]
    
        # Call store_url_dataset function
        train.store_url_dataset('meta-llama/Llama-2-7b-hf', args.s3_path, urls)
    
    if __name__ == "__main__":
        main()
    >>>>>>> updated
    ```
    
    Now, when you run the script with the `-h` or `--help` option, it will print out the usage information:
    
    ```bash
    python train_cli.py --help
    ```

--- train_cli.py
+++ train_cli.py
@@ -2,9 +2,12 @@ import argparse
 import train
 
 def main():
-    parser = argparse.ArgumentParser(description='Load URLs and store dataset.')
-    parser.add_argument('filename', type=str, help='File containing URLs')
-    parser.add_argument('s3_path', type=str, help='S3 bucket path')
+    parser = argparse.ArgumentParser(
+        description='This script loads URLs from a specified file and stores the dataset to a specified S3 bucket path.',
+        usage='python train_cli.py <filename> <s3_path>'
+    )
+    parser.add_argument('filename', type=str, help='The name of the file containing the URLs. Each URL should be on a new line.')
+    parser.add_argument('s3_path', type=str, help='The path to the S3 bucket where the dataset will be stored.')
     args = parser.parse_args()
 
     # Load URLs from file
