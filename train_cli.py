14e0c96b37d2d909e159d1e07c1fd4f38d9e6323
Jason Livesay <runvnc@gmail.com>
2023-11-07 02:43:30 +0000

    aider: Updated `train_cli.py` to be an executable script that reads a filename and S3 bucket path from the command line, loads the URLs into a list, and calls `store_url_dataset` from `train.py` passing in those URLs and the S3 path.
    # Aider chat conversation:
    
    USER: Please update the `train_cli.py` to be an executable script that reads a filename and s3 buck path from the command line (such as `url_list.txt`) and then loads those urls into an array/list, then calls `store_url_dataset` from `train.py` passing in those URLs and the s3 path. Thanks.
    
    ASSISTANT: To accomplish this, we need to modify the `train_cli.py` file. We will use the `argparse` library to handle command-line arguments. The script will read the filename and s3 bucket path from the command line, load the URLs into a list, and then call the `store_url_dataset` function from `train.py`.
    
    Here is the *edit block* for the changes:
    
    ```python
    train_cli.py
    <<<<<<< HEAD
    import train.py
    
    # read [file] and load data
    =======
    import argparse
    import train
    
    def main():
        parser = argparse.ArgumentParser(description='Load URLs and store dataset.')
        parser.add_argument('filename', type=str, help='File containing URLs')
        parser.add_argument('s3_path', type=str, help='S3 bucket path')
        args = parser.parse_args()
    
        # Load URLs from file
        with open(args.filename, 'r') as f:
            urls = [line.strip() for line in f]
    
        # Call store_url_dataset function
        train.store_url_dataset('meta-llama/Llama-2-7b-hf', args.s3_path, urls)
    
    if __name__ == "__main__":
        main()
    >>>>>>> updated
    ```
    
    This script can be run from the command line with two arguments: the filename containing the URLs and the S3 bucket path. For example:
    
    ```bash
    python train_cli.py url_list.txt my_s3_bucket_path
    ```

--- train_cli.py
+++ train_cli.py
@@ -1,4 +1,19 @@
-import train.py
+import argparse
+import train
 
-# read [file] and load data
+def main():
+    parser = argparse.ArgumentParser(description='Load URLs and store dataset.')
+    parser.add_argument('filename', type=str, help='File containing URLs')
+    parser.add_argument('s3_path', type=str, help='S3 bucket path')
+    args = parser.parse_args()
+
+    # Load URLs from file
+    with open(args.filename, 'r') as f:
+        urls = [line.strip() for line in f]
+
+    # Call store_url_dataset function
+    train.store_url_dataset('meta-llama/Llama-2-7b-hf', args.s3_path, urls)
+
+if __name__ == "__main__":
+    main()
 
