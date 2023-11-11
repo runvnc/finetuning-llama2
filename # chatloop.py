c0f6dc768387c51849a6cc3225479c1a9b30860c
Jason Livesay <runvnc@gmail.com>
2023-11-11 09:08:43 +0000

    aider: Create chatloop.py script that allows the user to ask questions and appends the input and response to the inputs list.
    # Aider chat conversation:
    
    USER: Examine how this program works. Note that inputs is list. Create another script chatloop.py that allows the user to ask questions and after each question appends the input as well as the response with role "assistant" and content = text of reply. The user inputs questions using the console. Don't display the model name, just make the user's input white and the model output green.
    
    ASSISTANT: To create the new script `chatloop.py` that you've described, we'll need to write a Python program that continuously accepts user input from the console, generates a response using the `generate_text` function, and appends both the user's input and the model's response to the `inputs` list. The user's input will have the role "user" and the model's response will have the role "assistant".
    
    Here's how the `chatloop.py` script will work:
    1. It will start a loop that runs indefinitely until the user decides to exit.
    2. It will prompt the user to enter a question.
    3. It will call the `generate_text` function with the user's input to get the model's response.
    4. It will append the user's input and the model's response to the `inputs` list with the appropriate roles.
    5. It will print the user's input in white and the model's response in green.
    6. It will repeat this process until the user exits the loop.
    
    Let's create the `chatloop.py` script:
    
    ```python
    # chatloop.py
    <<<<<<< SEARCH
    =======
    #!/usr/bin/env python
    
    from termcolor import colored
    from generate_text import generate_text
    
    def chat_loop():
        inputs = []  # Initialize the conversation history list
        while True:  # Start an infinite loop
            try:
                user_input = input(colored('You: ', 'white'))  # Get input from the user
                if user_input.lower() == 'exit':  # Allow the user to exit the loop
                    print("Exiting chat...")
                    break
    
                # Append the user's input to the conversation history
                inputs.append({"role": "user", "content": user_input})
    
                # Generate the model's response
                model_response = generate_text('aws13bchatz2', user_input)
    
                # Append the model's response to the conversation history
                inputs.append({"role": "assistant", "content": model_response})
    
                # Print the model's response
                print(colored('AI: ', 'green') + colored(model_response, 'green'))
    
            except KeyboardInterrupt:
                # Handle Ctrl+C gracefully
                print("\nExiting chat...")
                break
    
    if __name__ == '__main__':
        chat_loop()
    >>>>>>> REPLACE
    ```
    
    This script will be saved as `chatloop.py` in the same directory as your existing `test_inference_before_after.py` script. Please note that the `generate_text` function is assumed to be defined in the `generate_text.py` file, which should be in the same directory or in the Python path for this script to work correctly. If you need to modify the `generate_text` function or any other dependencies, please provide the necessary files.

---
+++ # chatloop.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from termcolor import colored
+from generate_text import generate_text
+
+def chat_loop():
+    inputs = []  # Initialize the conversation history list
+    while True:  # Start an infinite loop
+        try:
+            user_input = input(colored('You: ', 'white'))  # Get input from the user
+            if user_input.lower() == 'exit':  # Allow the user to exit the loop
+                print("Exiting chat...")
+                break
+
+            # Append the user's input to the conversation history
+            inputs.append({"role": "user", "content": user_input})
+
+            # Generate the model's response
+            model_response = generate_text('aws13bchatz2', user_input)
+
+            # Append the model's response to the conversation history
+            inputs.append({"role": "assistant", "content": model_response})
+
+            # Print the model's response
+            print(colored('AI: ', 'green') + colored(model_response, 'green'))
+
+        except KeyboardInterrupt:
+            # Handle Ctrl+C gracefully
+            print("\nExiting chat...")
+            break
+
+if __name__ == '__main__':
+    chat_loop()
