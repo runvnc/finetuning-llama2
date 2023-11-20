179d36598b1d3ab055f6113977c72a0243928eb0
Jason Livesay <runvnc@gmail.com>
2023-11-20 22:36:48 +0000

    aider: Added a CLI script called `manage_endpoints.py` that can list and stop SageMaker endpoints.
    # Aider chat conversation:
    
    USER: Please add a cli script (in python) called manage_endpoints that will have commands to A) list and B) stop endpoints that have been created with deploy.py
    
    ASSISTANT: To create a new CLI script called `manage_endpoints.py` that can list and stop SageMaker endpoints, we will need to:
    
    1. Create a new file `manage_endpoints.py`.
    2. Add the necessary imports and setup for a command-line interface using `argparse`.
    3. Add a function to list endpoints using the SageMaker client from `boto3`.
    4. Add a function to stop an endpoint, which will call the `stop_endpoint` function from `deploy.py`.
    5. Set up the command-line arguments to handle the 'list' and 'stop' commands.
    
    Here is the *SEARCH/REPLACE block* for the new file:
    
    manage_endpoints.py
    ```python
    <<<<<<< SEARCH
    =======
    import argparse
    import boto3
    import deploy
    
    def list_endpoints():
        sagemaker_client = boto3.client('sagemaker')
        endpoints = sagemaker_client.list_endpoints(SortBy='CreationTime', SortOrder='Descending')
        for endpoint in endpoints['Endpoints']:
            print(f"{endpoint['EndpointName']} (Status: {endpoint['EndpointStatus']})")
    
    def main():
        parser = argparse.ArgumentParser(description='Manage SageMaker endpoints.')
        subparsers = parser.add_subparsers(dest='command', help='Sub-command help')
    
        # Subparser for the 'list' command
        parser_list = subparsers.add_parser('list', help='List all SageMaker endpoints')
        parser_list.set_defaults(func=list_endpoints)
    
        # Subparser for the 'stop' command
        parser_stop = subparsers.add_parser('stop', help='Stop a SageMaker endpoint')
        parser_stop.add_argument('endpoint_name', type=str, help='The name of the endpoint to stop')
        parser_stop.set_defaults(func=lambda args: deploy.stop_endpoint(args.endpoint_name))
    
        args = parser.parse_args()
        if args.command is None:
            parser.print_help()
        else:
            args.func(args)
    
    if __name__ == '__main__':
        main()
    >>>>>>> REPLACE
    ```
    
    This block of code will create a new file `manage_endpoints.py` with a CLI that has two commands: `list` to list all SageMaker endpoints and `stop` to stop a specific endpoint. The `list_endpoints` function uses the `boto3` client to retrieve and print the list of endpoints. The `stop` command uses the `stop_endpoint` function from `deploy.py` to stop the specified endpoint.

---
+++ manage_endpoints.py
@@ -0,0 +1,31 @@
+import argparse
+import boto3
+import deploy
+
+def list_endpoints():
+    sagemaker_client = boto3.client('sagemaker')
+    endpoints = sagemaker_client.list_endpoints(SortBy='CreationTime', SortOrder='Descending')
+    for endpoint in endpoints['Endpoints']:
+        print(f"{endpoint['EndpointName']} (Status: {endpoint['EndpointStatus']})")
+
+def main():
+    parser = argparse.ArgumentParser(description='Manage SageMaker endpoints.')
+    subparsers = parser.add_subparsers(dest='command', help='Sub-command help')
+
+    # Subparser for the 'list' command
+    parser_list = subparsers.add_parser('list', help='List all SageMaker endpoints')
+    parser_list.set_defaults(func=list_endpoints)
+
+    # Subparser for the 'stop' command
+    parser_stop = subparsers.add_parser('stop', help='Stop a SageMaker endpoint')
+    parser_stop.add_argument('endpoint_name', type=str, help='The name of the endpoint to stop')
+    parser_stop.set_defaults(func=lambda args: deploy.stop_endpoint(args.endpoint_name))
+
+    args = parser.parse_args()
+    if args.command is None:
+        parser.print_help()
+    else:
+        args.func(args)
+
+if __name__ == '__main__':
+    main()
